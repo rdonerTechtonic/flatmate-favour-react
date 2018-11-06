e8d2063a88a3d024a6002f8184558a4c7e5b1488
Devin Giannoni <>
2018-11-06 14:02:12 -0700

    Created handleEditHouse function.  Passed this new function down as a prop to userBar.js.  Setup the editHouse button with an onClick that will fire the new function, toggling editHouse mode on.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 
 //holds entire dashboard page
 //dashboard includes logo, pictures of users with button functionality, and an event dashboard that holds all events
-export const Dashboard = ({ ffRoommates, ffEvents, handleUpdateEventStatus, handleDateChange }) => {
+export const Dashboard = ({ ffRoommates, ffEvents, handleUpdateEventStatus, handleDateChange, handleEditHouse }) => {
     return (
       <div>
         <Link to="/login"><img src="/Flatmate-Favour-Logo.png" alt="flatmate favour icon" id="logo" /></Link>
-          <UserBar ffRoommates={ffRoommates} />
+          <UserBar handleEditHouse={handleEditHouse} ffRoommates={ffRoommates} />
             <EventDashboard ffEvents={ffEvents} handleUpdateEventStatus={handleUpdateEventStatus} handleDateChange={handleDateChange} />
       </div>
     );

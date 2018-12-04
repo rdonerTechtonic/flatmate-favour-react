b67c723adfc4c6e0381f6469bd6e56fa52ce263e
Ryan Doner <43283040+rdonerTechtonic@users.noreply.github.com>
2018-12-04 16:11:43 -0700

    Update EventItem.js

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -11,10 +11,10 @@ export const EventItem = function (props) {
     let statusButtonText = '';
 
     switch (props.eventStatus) {
-      // case 'pending':
-      //   statusImg = '/Handshake-Icon.png';
-      //   statusButtonText = 'Accept?';
-      //   break;
+      case 'pending':
+        statusImg = '/Handshake-Icon.png';
+        statusButtonText = 'Accept?';
+        break;
       case 'accepted':
         statusImg = '/Hourglass-Icon.png';
         statusButtonText = 'Done?';
@@ -23,10 +23,10 @@ export const EventItem = function (props) {
         statusImg = '/Check-Icon.png';
         statusButtonText = 'Thank?';
         break;
-      // case 'thanked':
-      //   statusImg = '/Check-Icon.png';
-      //   statusButtonText = 'Thanked';
-      //   break;
+      case 'thanked':
+        statusImg = '/Check-Icon.png';
+        statusButtonText = 'Thanked';
+        break;
       default:
     }
 
